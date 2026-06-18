941ee59147e53b2e4027dc709210a8849a409cea
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-06-19 05:35:23 +0800

    Moved previous Ship, Gameboard and Player initialization into gameRound

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import { Gameboard } from "./gameboard.js";
 import { Ship } from "./ship.js";
 
 function gameRound() {
+  let ship = new Ship();
+  let gameboard = new Gameboard();
+  let player = new Player();
+
 }
 
