71d63d3daf075a34099554329869b9c16157c040
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-06-22 22:09:29 +0800

    Imports for gameboard.js

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,22 @@
 /* gameboard.js */
+import { createElement } from "./element_creator.js";
+import {
+  createP,
+  createA,
+  createDiv,
+  createButton,
+  createImg,
+  createForm,
+  createFieldset,
+  createLegend,
+  createLabel,
+  createInput,
+  createSelect,
+  createOption,
+  htmlOps,
+} from "./element_creator.js";
+import { Tree } from "./binary_search_tree.js";
+import { buttonPressed } from "./interactions.js";
 
 class Gameboard {
   constructor() {
