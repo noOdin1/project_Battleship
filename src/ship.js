83c20c2574b9fdeb60d2255bf61a5dcdc91fb568
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-07-11 03:07:18 +0800

    Ship constants for each ship dimensions and count

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,14 @@
 /* ship.js  */
 
+const shipDim = {
+  battleship: 4,
+  cruiser: 3,
+  destroyer: 2,
+  patrolBoat: 1,
+};
+
+const shipCount = { battleship: 1, cruiser: 2, destroyer: 3, patrolBoat: 4 };
+
 class Ship {
   constructor(pos, name) {
     console.log("Initializing Ship class");
