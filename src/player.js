d75d7f83437eeb89c2d6a6faafdfe6791a910f62
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-07-14 00:58:15 +0800

    Adding shipDim and shipCount to Player class
    Removing shipDim and shipCount from Ship class. These constants will be
    used to create ships with the name, dimension and the counts.

--- src/player.js
+++ src/player.js
@@ -2,10 +2,19 @@
 import { Gameboard } from "./gameboard.js";
 import { Ship } from "./ship.js";
 
+const shipDim = {
+  battleship: 4,
+  cruiser: 3,
+  destroyer: 2,
+  patrolBoat: 1,
+};
+
+const shipCount = { battleship: 1, cruiser: 2, destroyer: 3, patrolBoat: 4 };
+
 class Player {
   constructor() {
     console.log("Initializing Player class");
   }
 }
 
-export { Player };
+export { Player, shipDim, shipCount };
